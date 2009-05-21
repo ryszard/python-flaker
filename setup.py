2915819d4410d92c9c9f28192c9a909ceb914b07
Ryszard Szopa <ryszard.szopa@gmail.com>
2009-05-21 19:31:18 +0200

    Added setup.py.

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+import sys
+
+
+setup(
+    name = "flaker",
+    version = "0.1",
+    description="Flaker.pl client",
+    author="Ryszard Szopa",
+    author_email="ryszard.szopa@gmail.com",
+    url="http://github.com/ryszard/python-flaker/",
+    packages = find_packages(),
+    keywords='internet flaker api',
+    zip_safe=True,
+    long_description = """\
+Client library for flaker.pl's API. See http://flaker.pl and http://blog.flaker.pl/api/ for more information.""",
+    install_requires = ['simplejson'] if sys.version < 2.6 else [],
+    license = 'MIT',
+    package_data = {
+        '': ['*.txt', '*.rst', '*.markdown'],},
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Operating System :: OS Independent",
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Topic :: Internet",
+      ],
+
+
+)
